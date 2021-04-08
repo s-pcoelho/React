5e38087194e18e5d41461370290549a7b9944ea6
Sophia Coelho <76616748+s-pcoelho@users.noreply.github.com>
2021-04-08 16:44:44 -0300

    Creating a project structure

---
+++ ExemploExpo/src/Style.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native'
+
+export default StyleSheet.create({
+    textoDestaque:{
+        fontSize: 30,
+        color:'purple',
+
+    },
+    texto:{
+        fontSize: 25,
+        color:'purple',
+
+    },
+})
